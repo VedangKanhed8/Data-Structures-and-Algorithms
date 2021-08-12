603b5ac0277b628096d9f6350a64e701ccea78fe
VedangKanhed8 <67642576+VedangKanhed8@users.noreply.github.com>
2021-08-12 15:09:29 +0530

    Update Player with Max Score.cpp

--- Player with Max Score.cpp
+++ Player with Max Score.cpp
@@ -11,13 +11,13 @@ int find_max_score_for_player1(int* piles , int first_index , int last_index , b
   
     if(player1_will_move) {
         int take_first_pile = piles[first] + find_max_score_for_player1(piles , first_index + 1 , last_index , !player1_will_move);
-        int take_first_pile = piles[last] + find_max_score_for_player1(piles , first_index , last_index - 1, !player1_will_move);
+        int take_last_pile = piles[last] + find_max_score_for_player1(piles , first_index , last_index - 1, !player1_will_move);
         dp[first][last][player1_will_move] = max(take_first_pile,take_last_pile);
     }
   
     else {
         int take_first_pile = find_max_score_for_player1(piles , first_index + 1 , last_index , !player1_will_move);
-        int take_first_pile = find_max_score_for_player1(piles , first_index , last_index - 1, !player1_will_move);
+        int take_last_pile = find_max_score_for_player1(piles , first_index , last_index - 1, !player1_will_move);
         dp[first][last][player1_will_move] = min(take_first_pile,take_last_pile);
     }
   
